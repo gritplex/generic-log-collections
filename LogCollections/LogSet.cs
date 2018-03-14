b5ac8be5beb22086de70e967f6889e9432ee9b57
Matthias Grunewald <matthias.grunewald@gritplex.com>
2018-03-14 14:13:30 +0100

    moved common behavior in base class; added readonly mode for reading

--- LogCollections/LogSet.cs
+++ LogCollections/LogSet.cs
@@ -7,14 +7,13 @@ namespace LogCollections
 {
     public sealed class LogSet<T> : LogCollection<T>, ISet<T>
     {
-        private HashSet<T> _set;
-
         public LogSet(
             string name,
             int id,
             Func<T, Guid> keyProvider,
             Func<T, byte[]> serializer,
             Func<byte[], T> deserializer,
+            bool readOnly = false,
             long maxFileSize = sizeof(byte) * 1024 * 1024 * 25,
             int compactEvery = 100_000,
             EqualityComparer<T> comparer = null) 
@@ -24,28 +23,18 @@ namespace LogCollections
                   keyProvider,
                   serializer,
                   deserializer,
+                  readOnly,
                   maxFileSize,
                   compactEvery)
         {
-            _set = new HashSet<T>(comparer ?? EqualityComparer<T>.Default);
-
-            foreach (var entry in _log)
-            {
-                if ((entry.Meta & c_Remove) != 0)
-                {
-                    _set.Add(_deserializer(entry.Value));
-                }
-                else
-                {
-                    _set.Remove(_deserializer(entry.Value));
-                }
-            }
+            _internal = new HashSet<T>(comparer ?? EqualityComparer<T>.Default);
+            InitFromLog();
         }       
 
         #region Implementation of ISet<T>
-        public int Count => _set.Count;
+        public int Count => _internal.Count;
 
-        public bool IsReadOnly => false;
+        public bool IsReadOnly => _readOnly;
 
         public bool Add(T item)
         {
@@ -54,19 +43,19 @@ namespace LogCollections
 
             MaybeCompact();
 
-            return _set.Add(item);
+            return ((HashSet<T>)_internal).Add(item);
         }
         public void Clear() => throw new NotImplementedException();
-        public bool Contains(T item) => _set.Contains(item);
-        public void CopyTo(T[] array, int arrayIndex) => _set.CopyTo(array, arrayIndex);
-        public void ExceptWith(IEnumerable<T> other) => _set.ExceptWith(other);
-        public IEnumerator<T> GetEnumerator() => _set.GetEnumerator();
-        public void IntersectWith(IEnumerable<T> other) => _set.IntersectWith(other);
-        public bool IsProperSubsetOf(IEnumerable<T> other) => _set.IsProperSubsetOf(other);
-        public bool IsProperSupersetOf(IEnumerable<T> other) => _set.IsProperSupersetOf(other);
-        public bool IsSubsetOf(IEnumerable<T> other) => _set.IsSubsetOf(other);
-        public bool IsSupersetOf(IEnumerable<T> other) => _set.IsSupersetOf(other);
-        public bool Overlaps(IEnumerable<T> other) => _set.Overlaps(other);
+        public bool Contains(T item) => _internal.Contains(item);
+        public void CopyTo(T[] array, int arrayIndex) => _internal.CopyTo(array, arrayIndex);
+        public void ExceptWith(IEnumerable<T> other) => ((HashSet<T>)_internal).ExceptWith(other);
+        public IEnumerator<T> GetEnumerator() => _internal.GetEnumerator();
+        public void IntersectWith(IEnumerable<T> other) => ((HashSet<T>)_internal).IntersectWith(other);
+        public bool IsProperSubsetOf(IEnumerable<T> other) => ((HashSet<T>)_internal).IsProperSubsetOf(other);
+        public bool IsProperSupersetOf(IEnumerable<T> other) => ((HashSet<T>)_internal).IsProperSupersetOf(other);
+        public bool IsSubsetOf(IEnumerable<T> other) => ((HashSet<T>)_internal).IsSubsetOf(other);
+        public bool IsSupersetOf(IEnumerable<T> other) => ((HashSet<T>)_internal).IsSupersetOf(other);
+        public bool Overlaps(IEnumerable<T> other) => ((HashSet<T>)_internal).Overlaps(other);
         public bool Remove(T item)
         {
             var entry = new LogEntry(_id, _keyProvider(item), c_Remove, _serializer(item));
@@ -74,13 +63,13 @@ namespace LogCollections
 
             MaybeCompact();
 
-            return _set.Remove(item);
+            return _internal.Remove(item);
         }
-        public bool SetEquals(IEnumerable<T> other) => _set.SetEquals(other);
-        public void SymmetricExceptWith(IEnumerable<T> other) => _set.SymmetricExceptWith(other);
-        public void UnionWith(IEnumerable<T> other) => _set.UnionWith(other);
+        public bool SetEquals(IEnumerable<T> other) => ((HashSet<T>)_internal).SetEquals(other);
+        public void SymmetricExceptWith(IEnumerable<T> other) => ((HashSet<T>)_internal).SymmetricExceptWith(other);
+        public void UnionWith(IEnumerable<T> other) => ((HashSet<T>)_internal).UnionWith(other);
         void ICollection<T>.Add(T item) => this.Add(item);
-        IEnumerator IEnumerable.GetEnumerator() => _set.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _internal.GetEnumerator();
         #endregion
     }
 }
